36d30566898401bfc50853d38c715f09d5eb1619
Андрей Лютиков <70757449+andrey-andreu@users.noreply.github.com>
2021-12-07 01:46:24 +0300

    Delete find_max_substring_occurrence.py

--- project2/find_max_substring_occurrence.py
+++
@@ -1,7 +0,0 @@
-def find_max_substring_occurrence(input_string):
-    n = len(input_string)
-    for i in range(1, len(input_string) + 1):
-        a = input_string[:i]
-        s = input_string.split(a)
-        if s.count("") == n/len(a) + 1:
-            return int(n/len(a))
